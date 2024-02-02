345759fb504695eeeb4ad35d67583815fbdfd3c1
Tirapa Reddy TOndapu <tirapareddy.tondapu@kellogg.com>
2024-02-02 10:59:07 +0530

    Added Logs for Test Cases

--- AceTesting/src/test/java/SumApiTest.java
+++ AceTesting/src/test/java/SumApiTest.java
@@ -1,55 +1,67 @@
 import org.junit.Test;
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import com.aventstack.extentreports.ExtentTest;
-import org.junit.Test;
 
 public class SumApiTest extends BaseTest {
 
     private final String BASE_URL = "http://localhost:7800/sum_api/v1";
-
+    private static final Logger LOGGER = LoggerFactory.getLogger(SumApiTest.class);
     @Test
     public void testSumApione() {
+        // Create a test in the ExtentReports instance
+        ExtentTest test = extent.createTest("testSumApione", "Testing Sum API with numbers 2 and 2");
+
+        // Log test steps or information
+        test.info("Sending request to Sum API with numbers 2 and 2");
+        LOGGER.info("Sending request to Sum API with numbers 2 and 2");
         given()
             .contentType("application/json")
             .body("{\"number2\": 2, \"number1\": 2}")
         .when()
-            .post(BASE_URL+ "/sum")
+            .post(BASE_URL + "/sum")
         .then()
             .statusCode(200)
             .body("sum", equalTo(4))
             .body("prime", equalTo(false));
-        
-        ExtentTest test = extent.createTest("SampleTest", "This is a sample Extent Report test");
+
+        test.pass("API request successful");
+        LOGGER.info("API request successful");
+    }
+
+    @Test
+    public void testSumApi() {
+        // Create a test in the ExtentReports instance
+        ExtentTest test = extent.createTest("testSumApi", "Testing Sum API with numbers 9 and 2");
 
         // Log test steps or information
-        test.pass("Step 1: This is a pass step");
-        test.fail("Step 2: This is a fail step");
-    }
-    
-	@Test
-    public void testSumApi() {
+        test.info("Sending request to Sum API with numbers 9 and 2");
+        LOGGER.info("Sending request to Sum API with numbers 9 and 2");
         given()
             .contentType("application/json")
             .body("{\"number2\": 9, \"number1\": 2}")
         .when()
-            .post(BASE_URL+ "/sum")
+            .post(BASE_URL + "/sum")
         .then()
             .statusCode(200)
             .body("sum", equalTo(11))
             .body("prime", equalTo(true));
-        
-        ExtentTest test = extent.createTest("SampleTest", "This is a sample Extent Report test");
 
-        // Log test steps or information
-        test.pass("Step 1: This is a pass step");
-        test.fail("Step 2: This is a fail step");
+        test.pass("API request successful");
+        LOGGER.info("API request successful");
     }
-	
-	
+
     @Test
     public void testSubApi() {
+        // Create a test in the ExtentReports instance
+        ExtentTest test = extent.createTest("testSubApi", "Testing Sub API with numbers 2 and 2");
+
+        // Log test steps or information
+        test.info("Sending request to Sub API with numbers 2 and 2");
+        LOGGER.info("Sending request to Sub API with numbers 2 and 2");
         given()
             .contentType("application/json")
             .body("{\"number2\": 2, \"number1\": 2}")
@@ -59,17 +71,19 @@ public class SumApiTest extends BaseTest {
             .statusCode(200)
             .body("sum", equalTo(0))
             .body("prime", equalTo(false));
-        
-        ExtentTest test = extent.createTest("SampleTest", "This is a sample Extent Report test");
 
-        // Log test steps or information
-        test.pass("Step 1: This is a pass step");
-        test.fail("Step 2: This is a fail step");
+        test.pass("API request successful");
+        LOGGER.info("API request successful");
     }
-    
-    
+
     @Test
     public void testMulApi() {
+        // Create a test in the ExtentReports instance
+        ExtentTest test = extent.createTest("testMulApi", "Testing Mul API with numbers 2 and 2");
+
+        // Log test steps or information
+        test.info("Sending request to Mul API with numbers 2 and 2");
+        LOGGER.info("Sending request to Mul API with numbers 2 and 2");
         given()
             .contentType("application/json")
             .body("{\"number2\": 2, \"number1\": 2}")
@@ -79,12 +93,8 @@ public class SumApiTest extends BaseTest {
             .statusCode(200)
             .body("sum", equalTo(4))
             .body("prime", equalTo(false));
-        ExtentTest test = extent.createTest("SampleTest", "This is a sample Extent Report test");
 
-        // Log test steps or information
-        test.pass("Step 1: This is a pass step");
-        test.fail("Step 2: This is a fail step");
+        test.pass("API request successful");
+        LOGGER.info("API request successful");
     }
-    
-    
 }
---
+++ AceTesting/src/test/java/SumApiTwoTest.java
@@ -0,0 +1,46 @@
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+// other imports ...
+
+import com.aventstack.extentreports.ExtentTest;
+
+public class SumApiTwoTest extends BaseTest {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(SumApiTwoTest.class);
+    private final String BASE_URL = "http://localhost:7800/sum_api/v1";
+
+    @Test
+    public void testSumApione() {
+    	
+    	ExtentTest test = extent.createTest("testSumApione", "Testing Sum API with numbers 2 and 2");
+        
+        // rest of your test code ...
+        test.info("Sending request to Sum API with numbers 2 and 2");
+        LOGGER.info("Sending request to Sum API with numbers 2 and 2");
+        given()
+            .contentType("application/json")
+            .body("{\"number2\": 2, \"number1\": 2}")
+        .when()
+            .post(BASE_URL + "/sum")
+        .then()
+            .statusCode(200)
+            .body("sum", equalTo(4))
+            .body("prime", equalTo(false));
+
+        test.pass("API request successful");
+        LOGGER.info("API request successful");
+    }
+
+    @Test
+    public void testSumApi() {
+        LOGGER.info("Starting testSumApi");
+        // rest of your test code ...
+    }
+
+    // other test methods ...
+}
